d07852dcf0bde7d73e08e1cbb339e1d74a8cfa37
aman1398 <129157358+aman1398@users.noreply.github.com>
2024-03-10 08:20:45 +0000

    changes

---
+++ test.js
@@ -0,0 +1,47 @@
+"use strict" //run code as new version
+const id=23232323
+let namea="Aman"
+
+//console.log(id);
+//console.log(namea);
+//console.table([id,namea]);
+
+//console.log(typeof id);
+//console.log(typeof 0);
+
+// let code="1602a"
+// console.log(typeof code)
+// let codea=Number(code)
+// console.log(typeof codea)
+// console.log(codea);
+
+/* to convert data types use data type name with first letter in uppercase*/
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
+
+
+
+
+
+
